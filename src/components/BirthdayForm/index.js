c32fd7a76b4156a226e657bb124fd2a9e1491890
Hilton1 <josehilton02@gmail.com>
2023-05-01 22:46:14 -0300

    Adicionando button

--- src/components/BirthdayForm/index.js
+++ src/components/BirthdayForm/index.js
@@ -1,25 +1,37 @@
 import FormGroup from '../FormGroup';
-import { Container } from './styles';
+import { ButtonContainer, Container, FormGroupContainer } from './styles';
 
 import Input from '../Input';
+
+import arrow from '../../assets/images/icon-arrow.svg';
 
 export default function BirthdayForm() {
   return (
     <Container>
-      <FormGroup>
-        <p>DAY</p>
-        <Input placeholder="DD" />
-      </FormGroup>
+      <FormGroupContainer>
+        <FormGroup>
+          <p>DAY</p>
+          <Input placeholder="DD" />
+        </FormGroup>
 
-      <FormGroup>
-        <p>MONTH</p>
-        <Input placeholder="MM" />
-      </FormGroup>
+        <FormGroup>
+          <p>MONTH</p>
+          <Input placeholder="MM" />
+        </FormGroup>
 
-      <FormGroup>
-        <p>YEAR</p>
-        <Input placeholder="YYYY" />
-      </FormGroup>
+        <FormGroup>
+          <p>YEAR</p>
+          <Input placeholder="YYYY" />
+        </FormGroup>
+      </FormGroupContainer>
+
+      <ButtonContainer>
+        <div className="line" />
+
+        <button type="button">
+          <img src={arrow} alt="arrow" />
+        </button>
+      </ButtonContainer>
 
     </Container>
   );
--- src/components/BirthdayForm/styles.js
+++ src/components/BirthdayForm/styles.js
@@ -1,9 +1,39 @@
 import styled from 'styled-components';
 
 export const Container = styled.form`
-  margin: 24px 0;
-  display: flex;
+margin: 24px 0;
 `;
 
 export const FormGroupContainer = styled.div`
+display: flex;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 24px;
+
+  .line {
+    height: 2px;
+    width: 100%;
+    background-color: hsl(0, 0%, 86%);
+  }
+
+  button {
+    border: none;
+    background: none;
+
+    img {
+      padding: 24px;
+      width: 88px;
+      border-radius: 100%;
+      background: hsl(259, 100%, 65%);
+
+      transition: all 200ms ease-in;
+
+      :hover {
+        background: hsl(0, 0%, 8%);
+      }
+    }
+  }
 `;
