4845c5832aaaacfdf6eda6816e4dcb00c068c86c
Hilton1 <josehilton02@gmail.com>
2023-05-01 21:39:24 -0300

    Refatorando BirthdayForm

--- src/components/BirthdayForm/index.js
+++ src/components/BirthdayForm/index.js
@@ -1,27 +1,25 @@
 import FormGroup from '../FormGroup';
-import { Container, FormGroupContainer } from './styles';
+import { Container } from './styles';
 
 import Input from '../Input';
 
 export default function BirthdayForm() {
   return (
     <Container>
-      <FormGroupContainer>
-        <FormGroup>
-          <p>DAY</p>
-          <Input placeholder="DD" />
-        </FormGroup>
+      <FormGroup>
+        <p>DAY</p>
+        <Input placeholder="DD" />
+      </FormGroup>
 
-        <FormGroup>
-          <p>MONTH</p>
-          <Input placeholder="MM" />
-        </FormGroup>
+      <FormGroup>
+        <p>MONTH</p>
+        <Input placeholder="MM" />
+      </FormGroup>
 
-        <FormGroup>
-          <p>YEAR</p>
-          <Input placeholder="YYYY" />
-        </FormGroup>
-      </FormGroupContainer>
+      <FormGroup>
+        <p>YEAR</p>
+        <Input placeholder="YYYY" />
+      </FormGroup>
 
     </Container>
   );
--- src/components/BirthdayForm/styles.js
+++ src/components/BirthdayForm/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.form`
   margin: 24px 0;
+  display: flex;
 `;
 
 export const FormGroupContainer = styled.div`
-  display: flex;
 `;
